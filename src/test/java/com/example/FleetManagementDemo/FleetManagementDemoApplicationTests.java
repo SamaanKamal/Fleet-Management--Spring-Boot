778de02435d2bec2a76328aced6d0a9c1c1eb0e2
Samaan Kamal <samaankamal72@gmail.com>
2024-07-16 23:13:38 +0300

    intialized project with minimum dependncies

---
+++ src/test/java/com/example/FleetManagementDemo/FleetManagementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FleetManagementDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
