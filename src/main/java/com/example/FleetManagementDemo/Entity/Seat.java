078d06e08aba59718a0b27ac59b4e2472a8b3749
Samaan Kamal <samaankamal72@gmail.com>
2024-07-17 23:34:46 +0300

    added seats entity to represent the seats in a trip using bus

--- src/main/java/com/example/FleetManagementDemo/Entity/Seat.java
+++ src/main/java/com/example/FleetManagementDemo/Entity/Seat.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 public class Seat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "seat_number", nullable = false)
