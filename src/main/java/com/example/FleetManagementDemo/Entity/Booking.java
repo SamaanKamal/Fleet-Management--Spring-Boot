4542702327485b0987467b30d54b23aac185ba88
Samaan Kamal <samaankamal72@gmail.com>
2024-07-17 23:36:41 +0300

    added booking entity to represent the bookings for a trip using bus and seats

---
+++ src/main/java/com/example/FleetManagementDemo/Entity/Booking.java
@@ -0,0 +1,62 @@
+package com.example.FleetManagementDemo.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "seat_number", nullable = false)
+    private int seatNumber;
+
+    public Booking() {
+    }
+
+    public Booking(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Trip getTrip() {
+        return trip;
+    }
+
+    public void setTrip(Trip trip) {
+        this.trip = trip;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+}
