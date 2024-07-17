470d83efcffd6f04ef3571b112e71e006d22c63c
Samaan Kamal <samaankamal72@gmail.com>
2024-07-17 22:58:30 +0300

    added city entity to represent the cities of the country

---
+++ src/main/java/com/example/FleetManagementDemo/Entity/City.java
@@ -0,0 +1,39 @@
+package com.example.FleetManagementDemo.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cities")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public City() {
+    }
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
