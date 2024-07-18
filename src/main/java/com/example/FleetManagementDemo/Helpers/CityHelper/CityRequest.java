d84127132c1188dd44abb1255f0bf6b5a193840b
Samaan Kamal <samaankamal72@gmail.com>
2024-07-18 20:59:57 +0300

    added city request helper class to act as request object for adding new ciites into the system

---
+++ src/main/java/com/example/FleetManagementDemo/Helpers/CityHelper/CityRequest.java
@@ -0,0 +1,17 @@
+package com.example.FleetManagementDemo.Helpers.CityHelper;
+
+public class CityRequest {
+
+    private String name;
+
+    public CityRequest() {
+    }
+
+    public CityRequest(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
