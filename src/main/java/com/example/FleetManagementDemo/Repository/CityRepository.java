9787bc30d8d6eb425adb804aa57779cef308672c
Samaan Kamal <samaankamal72@gmail.com>
2024-07-18 21:13:41 +0300

    added find by name method in city repository to preform select query on cities using name

--- src/main/java/com/example/FleetManagementDemo/Repository/CityRepository.java
+++ src/main/java/com/example/FleetManagementDemo/Repository/CityRepository.java
@@ -3,5 +3,8 @@ package com.example.FleetManagementDemo.Repository;
 import com.example.FleetManagementDemo.Entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CityRepository extends JpaRepository<City,Long> {
+    Optional<City> findByName(String name);
 }
