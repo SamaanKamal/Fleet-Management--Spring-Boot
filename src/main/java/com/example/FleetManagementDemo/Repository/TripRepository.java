1c02b2f0842c2397175824a3b19818f3c7f449f2
Samaan Kamal <samaankamal72@gmail.com>
2024-07-20 19:44:11 +0300

    fixed a bug of extending jpa reopsitory with entity name

--- src/main/java/com/example/FleetManagementDemo/Repository/TripRepository.java
+++ src/main/java/com/example/FleetManagementDemo/Repository/TripRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface TripRepository extends JpaRepository<TripRepository,Long> {
+public interface TripRepository extends JpaRepository<Trip,Long> {
     List<Trip> findByStartCityIdAndEndCityId(Long startCityId, Long endCityId);
 }
