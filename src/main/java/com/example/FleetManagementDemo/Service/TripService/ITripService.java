481ef23a2b5e3e6d3cb97004a26ebf1bfbe59022
Samaan Kamal <samaankamal72@gmail.com>
2024-07-20 19:40:24 +0300

    added new trip service interface contianing the CRUD methods needed for trip class

---
+++ src/main/java/com/example/FleetManagementDemo/Service/TripService/ITripService.java
@@ -0,0 +1,22 @@
+package com.example.FleetManagementDemo.Service.TripService;
+
+import com.example.FleetManagementDemo.Entity.Trip;
+import com.example.FleetManagementDemo.Helpers.TripHelper.TripRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ITripService {
+
+    Trip createTrip(TripRequest tripRequest);
+
+
+    Trip getTripDetails(Long id);
+
+    List<Trip> findTripsByOriginAndDestination(Long originId, Long destinationId);
+
+    Trip updateTrip(TripRequest tripRequest,Long id);
+
+    boolean deleteTrip(Long id);
+}
