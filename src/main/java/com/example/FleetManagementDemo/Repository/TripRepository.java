a7fb33ad8a38c21dae6026781964a4b528089a00
Samaan Kamal <samaankamal72@gmail.com>
2024-07-20 19:29:54 +0300

    added a method for querying the trips with start city and end city using their id

--- src/main/java/com/example/FleetManagementDemo/Repository/TripRepository.java
+++ src/main/java/com/example/FleetManagementDemo/Repository/TripRepository.java
@@ -1,6 +1,10 @@
 package com.example.FleetManagementDemo.Repository;
 
+import com.example.FleetManagementDemo.Entity.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TripRepository extends JpaRepository<TripRepository,Long> {
+    List<Trip> findByStartCityIdAndEndCityId(Long startCityId, Long endCityId);
 }
