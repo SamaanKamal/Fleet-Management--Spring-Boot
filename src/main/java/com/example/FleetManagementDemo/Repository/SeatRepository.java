6b3ab428d29709dcbddfefa40d638e5f19182bb1
Samaan Kamal <samaankamal72@gmail.com>
2024-07-21 21:06:25 +0300

    fixed method to get list of seats based on bus id in seat repository

--- src/main/java/com/example/FleetManagementDemo/Repository/SeatRepository.java
+++ src/main/java/com/example/FleetManagementDemo/Repository/SeatRepository.java
@@ -3,6 +3,8 @@ package com.example.FleetManagementDemo.Repository;
 import com.example.FleetManagementDemo.Entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SeatRepository extends JpaRepository<Seat,Long> {
-    Seat getAllSeatsByBusId(Long busId);
+    List<Seat> getAllSeatsByBusId(Long busId);
 }
