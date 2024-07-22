f04fd415ca142cca96ff3a9b490ea2d634cf0efa
Samaan Kamal <samaankamal72@gmail.com>
2024-07-22 22:54:07 +0300

    added needed methods for booking service class

--- src/main/java/com/example/FleetManagementDemo/Repository/StopRepository.java
+++ src/main/java/com/example/FleetManagementDemo/Repository/StopRepository.java
@@ -3,5 +3,8 @@ package com.example.FleetManagementDemo.Repository;
 import com.example.FleetManagementDemo.Entity.Stop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StopRepository extends JpaRepository<Stop,Long> {
+    List<Stop> findByTripIdOrderByStopOrderAsc(Long tripId);
 }
