8976de24cfa4c5c45b4e5d01c2a46e0b7bc31983
Samaan Kamal <samaankamal72@gmail.com>
2024-07-26 20:55:08 +0300

    added hidden file containing the info for configuration to mysql database connection and connected it to application.properties file

--- src/main/java/com/example/FleetManagementDemo/FleetManagementDemoApplication.java
+++ src/main/java/com/example/FleetManagementDemo/FleetManagementDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.FleetManagementDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:env.properties")
 public class FleetManagementDemoApplication {
 
 	public static void main(String[] args) {
